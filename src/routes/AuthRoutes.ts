014d1e2c37a3b98f23a836b75a0b0c2678830cdb
Aethan Ilagan <aethanilagan@gmail.com>
2020-05-03 13:48:46 +0800

    Adds return type

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -8,27 +8,32 @@ interface RequestBody extends Request {
   }
 }
 
-router.post('/confirmation', (req: RequestBody, res: Response) => {
-  const { email } = req.body
+router.post(
+  '/confirmation',
+  (req: RequestBody, res: Response): Response => {
+    const { email } = req.body
 
-  if (email) {
-    console.log(`Email just registered: ${email}`)
-    return res.status(200).send({ message: 'Email sent' })
+    if (email) {
+      console.log(`Email just registered: ${email}`)
+      return res.status(200).send({ message: 'Email sent' })
+    }
+    return res.status(422).send({ message: 'No email provided' })
   }
+)
 
-  return res.status(422).send({ message: 'No email provided' })
-})
+router.post(
+  '/login-request',
+  (req: RequestBody, res: Response): Response => {
+    const { email } = req.body
 
-router.post('/login-request', (req: RequestBody, res: Response) => {
-  const { email } = req.body
+    console.log(req.body)
 
-  console.log(req.body)
-
-  if (email) {
-    console.log('Login request sent')
-    return res.status(200).send({ message: 'mailer sent' })
+    if (email) {
+      console.log('Login request sent')
+      return res.status(200).send({ message: 'mailer sent' })
+    }
+    return res.status(422).send({ message: 'No email provided' })
   }
-  return res.status(422).send({ message: 'No email provided' })
-})
+)
 
 export { router }
